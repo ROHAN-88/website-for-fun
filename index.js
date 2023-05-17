0ba7d233a501eace6542a004d8b12836d22cd08a
Rohan <therohanshrestha6655@gmail.com>
2023-05-17 18:42:52 +0545

    6:42

--- index.js
+++ index.js
@@ -21,33 +21,14 @@ function moveDiv(event) {
 
 //The above source-code is for the moveable div 
 
+$(".section-1-1").click(function (){
+  alert("video is played");
+})
 
-function hello(){
-  console.log("hello");
-  alert("Video is played");
-}
 
-//this is for owl coursoul
-// var owl = $('.owl-carousel');
-// owl.owlCarousel({
-//     loop:false,
-//     nav:true,
-//     margin:10,
-//     responsive:{
-//         0:{
-//             items:1
-//         },
-//         600:{
-//             items:3
-//         },            
-//         960:{
-//             items:5
-//         },
-//         1200:{
-//             items:6
-//         }
-//     }
-// });
+
+
+
 
 $('.owl-carousel').owlCarousel({
   loop:false,
